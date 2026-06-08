a7ea63818153712fb0966c0a8ac22a9570c7666f
Stefan Dolete <m.dolete@studenti.unibg.it>
2026-06-08 12:35:16 +0200

    Update utenti.php

--- src/utenti.php
+++ src/utenti.php
@@ -137,7 +137,7 @@ function renderDettaglioUtente($pdo, $idUtente, $tab_corrente, $isAjax)
         $stmt->execute($params);
 
         $testoPerPagina = ($totale > $limit) ? " (<strong>$limit</strong> per pagina)" : "";
-        echo "<div class='table-top-bar'><p class='zero-margin'>Gruppi a cui appartiene l'utente trovati: <strong>$totale</strong> {$testoPerPagina}</p></div>";
+        echo "<div class='table-top-bar'><p class='info-risultati zero-margin'>Trovati <strong>$totale</strong> gruppi a cui appartiene l'utente {$testoPerPagina}</p></div>";
         
         if ($totale > 0) {
             $dati = [];
